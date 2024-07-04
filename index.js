1fe519737a2ecd37628da3a1e79b68daad19e4ab
manas-shinde <manas.a.shinde11@gmail.com>
2024-07-04 16:34:22 +0530

    feat : modifying brush size on user input.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const canvas = document.querySelector("#draw");
+const brushSize = document.getElementById("brushSize");
 const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
@@ -7,19 +8,19 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = "#BADASS";
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
-ctx.lineWidth = 100;
+// ctx.lineWidth = 100;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let hue = 0;
-let direction = true;
+// let direction = true;
 
 function draw(e) {
   if (!isDrawing) return;
 
-  console.log(e);
-
+  // console.log(e);
+  ctx.lineWidth = brushSize.value;
   ctx.strokeStyle = `hsl(${hue},100%,50%)`;
   ctx.beginPath();
   ctx.moveTo(lastX, lastY);
@@ -28,9 +29,9 @@ function draw(e) {
 
   [lastX, lastY] = [e.offsetX, e.offsetY];
 
-  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) direction = !direction;
+  // if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) direction = !direction;
 
-  ctx.lineWidth = direction ? ctx.lineWidth + 1 : ctx.lineWidth - 1;
+  // ctx.lineWidth = direction ? ctx.lineWidth + 1 : ctx.lineWidth - 1;
 
   hue = hue >= 360 ? 0 : hue + 1;
 }
@@ -42,3 +43,4 @@ canvas.addEventListener("mousedown", (e) => {
 });
 canvas.addEventListener("mouseup", () => (isDrawing = false));
 canvas.addEventListener("mouseout", () => (isDrawing = false));
+// brushSize.addEventListener("change",()=> );
